64f28cc04fa5d005d5a9e268f9f9c1e96140de2e
nesrine zarrouk <nesrinezarrouk83@gmail.com>
2023-02-04 16:42:24 +0100

    update 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+
+import { Component, AfterViewInit } from '@angular/core';
+import * as anime from 'animejs';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit{
+  
+  ngAfterViewInit(): void {
+
+    const container = document.querySelector('.container')
+        for (var i=0;i<=100;i++){
+            const blocks = document.createElement('div');
+            blocks.classList.add('blocks');
+            container!.appendChild(blocks);
+        }
+
+        function animateBlocks(){
+            anime({
+                targets: '.blocks',
+                translateX: function(){
+                    return anime.random(-700,700);
+                },
+                translateY: function(){
+                    return anime.random(-500,500);
+                },
+                scale: function(){
+                    return anime.random(1,5);
+                },
+
+                easing: 'linear',
+                duration: 3000,
+                delay: anime.stagger(10),
+                complete: animateBlocks,
+            })
+        }
+
+        animateBlocks()
+  }
+
+  
+
+
 
 }
