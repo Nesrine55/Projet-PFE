25cb37bf82cc034fe8f30f3d2574130d3b3b3e56
nesrine zarrouk <nesrinezarrouk83@gmail.com>
2023-02-15 09:54:28 +0100

    feat : changement de design page sign up et page login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,6 +12,17 @@ export class LoginComponent {
 
 
   public ngOnInit(){
+    const signUpButton: HTMLElement = document.getElementById('signUp')!;
+    const signInButton: HTMLElement = document.getElementById('signIn')!;
+    const container: HTMLElement = document.getElementById('container')!;
+    
+    signUpButton.addEventListener('click', () => {
+      container.classList.add("right-panel-active");
+    });
+    
+    signInButton.addEventListener('click', () => {
+      container.classList.remove("right-panel-active");
+    });
     $(document).ready(function () {
 
       'use strict';
